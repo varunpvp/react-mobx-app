180c6f7a20fd96ba95fa11e913a3bca4bc3415f3
Varun Pujari <varunpvp@gmail.com>
2024-06-22 13:08:10 +0530

    fix base url issue

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import packageJson from "./package.json";
 
 export default defineConfig(() => {
   return {
+    base: packageJson.homepage,
     build: {
       outDir: "build",
     },
